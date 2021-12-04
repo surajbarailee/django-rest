042525f8f91d3a8948e6372384aabe89b25d50ee
Suraj Baraili <baraileesuraj@gmail.com>
2021-12-04 22:05:56 +0545

    authentication and permission

--- dummyProject/settings.py
+++ dummyProject/settings.py
@@ -54,7 +54,9 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
-        'PAGE_SIZE': 2
+        'PAGE_SIZE': 2,
+        'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication'],
+        'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated','rest_framework.permissions.DjangoModelPermissions']
 }
 
 
